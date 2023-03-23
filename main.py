7ab252c4ff0b9faf3e0e09fad09990044c895c95
Victor Maru <vmaru@upei.ca>
2023-03-23 12:19:21 -0300

    script unzips, parses and blasts appropriately; revising blast XML parse for Entrez search and retrieve

--- main.py
+++ main.py
@@ -1,48 +1,93 @@
 # FIND RELATED SPECIES; IDENTIFY POLYMORPHISMS
-# Parse fasta/fastq file
-# Blast output to NCBI
-# Write result to xml file
-# Sort alignment scores; filter at e = 1e-7 (approx. 80% bit score)
 
 import os
-import zipfile
+import gzip
+from Bio import Entrez
 from Bio import SeqIO  # file parser
 from Bio.Blast import NCBIWWW  # Blast to the web and back
-from Bio import SearchIO  # sort and filter blast output
+from Bio.Blast import NCBIXML  # sort and filter blast output
 
-NCBIWWW.email = "maruyinks@gmail.com"
-fastq_dir = "file/FAT81153_pass_944e9f34_212.fastq"
-fastqzip_dir = "file/test_fastq.gz.zip"
+Entrez.email = 'vmaru01@outlook.com'
+NCBIWWW.email = 'vmaru01@outlook.com'
+fastq_dir = '/Users/springerlab/Desktop/Nanopore Test Data/20220817-sla-luc/no_sample/20220817_1750_MC-113190_FAT81153_9a4aacb0/fastq_pass'
 
-# unzip folder for BLAST
-if zipfile.is_zipfile(fastqzip_dir):  # verify that file is zipped
-    with zipfile.ZipFile(fastqzip_dir, "r") as zipped_file:
-        for filename in zipped_file.namelist():  # list zipped file content
-            print(f"Printing content in zipped_file...{filename}")
-            if filename.endswith(".fastq.gz"):  # verify that you're working with a file
-                print("Checking file format...Correct match!")
-                file = zipped_file.read(filename)
-                print("")
-                # OUT: BLAST - Parse file, index sequences and blast using sequence index
-                records = list(SeqIO.parse(file, "fastq"))
-                blast_handle = NCBIWWW.qblast("blastn", "nt", records[5].seq)
-            else:
-                print("Not a file")
-else:
-    print("Not a zipped file")
-
-# IN: XML - write BLAST result to xml file
-with open("test_blast.xml", "w") as blast_xml:
-    blast_xml.write(blast_handle.read())  # use str result as write object
-blast_handle.close()
+# To BLAST and Back
+i = 0
+for item in os.listdir(fastq_dir):
+    while i == 0:
+        try:
+            if item.lower().endswith(".gz"):
+                fullpath = os.path.join(fastq_dir, item)
+                print(fullpath)
+                with gzip.open(fullpath, 'rt') as file:
+                    records = SeqIO.parse(file, "fastq")
+                    for index, line in enumerate(records):
+                        if index == 0:
+                            print(f'Sequence Length: {len(line.seq)}\nSequence: {line.seq}')
+                            blast_handle = NCBIWWW.qblast("blastn", "nt", line.seq)
+                    with open("test_blast.xml", "w") as blast_xml:
+                        blast_xml.write(blast_handle.read())
+                        blast_handle.close()
+        except TypeError:
+            print("Error: Wrong file type")
+            pass
+        i += 1
 
 # Filter
-with open("test_blast.xml", "r") as xml_read:
-    blast_result = SearchIO.parse("test_blast.xml", "blast-xml")
+with open("test_blast.xml") as xml_read:
+    blast_result = NCBIXML.parse(xml_read)
     eval_threshold = float(input("Enter the e-value to filter by (ex. 2.3e-4): "))
-    for alignment in blast_result:
-        print(alignment)
+    for alignment in blast_result.alignments:
         for hsp in alignment.hsps:
+            print(hsp.id)
+            print(hsp.score)
+            print(hsp.identities)
             if hsp.evalue < eval_threshold:
-                print(f"HSP: {hsp}")
-                print("---------------------"*4)
+                print(hsp)   # revise
+
+# entrez_handle = Entrez.esearch(db='Nucleotide')
+
+# Filter
+# with open("test_blast.xml", "r") as xml_read:
+#     blast_result = SearchIO.parse("test_blast.xml", "blast-xml")
+#     eval_threshold = float(input("Enter the e-value to filter by (ex. 2.3e-4): "))
+#     for alignment in blast_result:
+#         print(alignment)
+#         for hsp in alignment.hsps:
+#             if hsp.evalue < eval_threshold:
+#                 print(f"HSP: {hsp}")
+#                 print("---------------------"*4)
+
+# # unzip folder for BLAST
+# if zipfile.is_zipfile(fastqzip_dir):  # zipped file check
+#     with zipfile.ZipFile(fastqzip_dir, "r") as zipped_file:
+#         for filename in zipped_file.namelist():  # list zipped file content
+#             print(f".namelist(): {zipped_file.namelist()} \nCurrent filename: {filename}")
+#             if filename.lower().endswith((".fastq.gz", "fasta")):  # fastq, fasta file check
+#                 print("Checking file format...file matches")
+#                 file = filename.read()
+#                 # OUT: BLAST - Parse file, index sequences and blast using sequence index
+#                 #
+#                 #
+#             else:
+#                 print("Not a file")
+# else:
+#     print("Not a zipped file")
+#
+#                 print("---------------------"*4)
+#
+# # unzip folder for BLAST
+# if zipfile.is_zipfile(fastqzip_dir):  # zipped file check
+#     with zipfile.ZipFile(fastqzip_dir, "r") as zipped_file:
+#         for filename in zipped_file.namelist():  # list zipped file content
+#             print(f".namelist(): {zipped_file.namelist()} \nCurrent filename: {filename}")
+#             if filename.lower().endswith((".fastq.gz", "fasta")):  # fastq, fasta file check
+#                 print("Checking file format...file matches")
+#                 file = filename.read()
+#                 # OUT: BLAST - Parse file, index sequences and blast using sequence index
+#                 #
+#                 #
+#             else:
+#                 print("Not a file")
+# else:
+#     print("Not a zipped file")
