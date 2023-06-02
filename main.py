a27fd2f2f1c9758a689dcef6c287176ce9519cd4
vmaru01 <vmaru@upei.ca>
2023-06-02 20:33:39 -0300

    evalue sort stable. Still working entrez

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from Bio import Entrez
 from Bio import SearchIO
 from Bio import SeqIO  # file parser
 from Bio.Blast import NCBIWWW  # Blast to the web and back
-from Bio.Blast import NCBIXML  # sort and filter blast output
+# from Bio.Blast import NCBIXML  # sort and filter blast output
 
 Entrez.email = 'vmaru01@outlook.com'
 NCBIWWW.email = 'vmaru01@outlook.com'
@@ -42,16 +42,28 @@ for item in blast_result:
     print(item)
     for hsp in item.hsps:
         if hsp.evalue < eval_threshold:
+            # taxids = item.accession.split('.')[0]
+            # print(f"TAXID: {taxids}")
             print(hsp)
             print(f"BITSCORE: {hsp.bitscore}")
             print(f"HIT ID: {hsp.hit_id}")
             print(f"EVALUE: {hsp.evalue}")
-            print("**********" * 4)
-          # revise
+            print("**********" * 6)
+        # revise
 xml_read.close()
 
-#ENTREZ
-handle = Entrez.esearch(db="nucleotide", term="txid63221")
-record = Entrez.read(handle)
-for result in record:
-    print(record)
+# ENTREZ
+search_handle = Entrez.esearch(db="taxonomy", term="63221")
+search_record = Entrez.read(search_handle)
+search_handle.close()
+print(search_record)
+
+link_handle = Entrez.elink(db="taxonomy", id=search_record['IdList'][0])
+link_record = Entrez.read(link_handle)
+search_handle.close()
+print(link_record)
+
+post_handle = Entrez.esummary(db="taxonomy", id=link_record[0]['LinkSetDb'][0]['Link'][0])
+post_record = Entrez.parse(post_handle)
+search_handle.close()
+print(post_record)
